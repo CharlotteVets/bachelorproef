f57762581e1a7aeccb6632c6bd0c119960c9b736
MaximV <maxim.vebruggen@gmail.com>
2015-04-28 17:48:52 +0200

    constructer klant

--- Klant.java
+++ Klant.java
@@ -6,6 +6,12 @@ private int telefoonnummer;
 private String emailadres;
 
 
+public Klant(String aNaam, int aTelefoonnummer, Sting aEmail, Adres aAdres){
 
+klantID = 
+naam = aNaam;
+telefoonnummer = aTelefoonnummer;
+emailadres = aEmail;
+}
 	
 }
